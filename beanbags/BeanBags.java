774b494f0a253f82bfc7ae9f7f241ff91aa57846
Sarah Joy Brittain <S.brittain-16@student.lboro.ac.uk>
2020-02-25 18:10:41 +0000

    hello

--- beanbags/BeanBags.java
+++ beanbags/BeanBags.java
@@ -25,16 +25,10 @@ public class BeanBags {
      * @param monthManufactured month of manufacture
      * @param quantity          number of bean bags
      */
-    public BeanBags(String id, String name, String manufacturer, short yearManufactured, byte monthManufactured,
-                    int quantity) {
-        this.id = id;
-        this.name = name;
-        this.manufacturer = manufacturer;
-        this.yearManufactured = yearManufactured;
-        this.monthManufactured = monthManufactured;
-        this.quantity = quantity;
-        this.quantityUnreserved = quantity;
-        this.additionalText = "";
+    public BeanBags (String id, String name, String manufacturer, short yearManufactured,
+                     byte monthManufactured, int quantity) throws IllegalIDException,
+                     InvalidMonthException{ this(id, name, manufacturer, yearManufactured,
+                     monthManufactured, quantity, "");
     }
 
     /**
@@ -46,8 +40,19 @@ public class BeanBags {
      * @param monthManufactured month of manufacture
      * @param quantity          number of bean bags
      */
-    public BeanBags(String id, String name, String manufacturer, short yearManufactured, byte monthManufactured,
-                    int quantity, String additionalText) {
+    public BeanBags(String id, String name, String manufacturer, short yearManufactured,
+                    byte monthManufactured, int quantity, String additionalText)throws
+                    IllegalIDException, InvalidMonthException, IllegalNumberOfBeanBagsAddedException
+                    {
+
+        checkId(id);
+        if (quantity<=0){
+            throw new IllegalNumberOfBeanBagsAddedException("Must add at least one beanbag");
+        }
+        if (monthManufactured < 1 || monthManufactured > 13){
+            throw new InvalidMonthException("Month has to be a number between 1 and 12");
+        }
+
         this.id = id;
         this.name = name;
         this.manufacturer = manufacturer;
@@ -64,6 +69,14 @@ public class BeanBags {
      */
     public String getId() {
         return id;
+    }
+
+    /**
+     *
+     */
+    public void setId(String id) throws IllegalIDException {
+        checkId(id);
+        this.id = id;
     }
 
     /**
@@ -140,7 +153,31 @@ public class BeanBags {
         additionalText = text;
     }
 
+    private void checkId(String id) throws IllegalIDException{
+        //  checks if the string is positive,
+        int hexadecimalNumber;
+        //  checks if the string is a hexadecimal number.
+        try{
+            hexadecimalNumber = Integer.parseInt(id,16);
+        }
+        catch(NumberFormatException e){
+            throw new IllegalIDException("ID must be hexadecimal number");
+        }
 
+        //  checks if the ID is positive
+        if (hexadecimalNumber < 0 ){
+            throw new IllegalIDException("ID must be positive");
+        }
+        // checks that 8 digits do not include a plus symbol.
+        if (id.startsWith("+")){
+            throw new IllegalIDException("ID must not contain + symbol");
+        }
+
+        // checks if the ID is 8 characters long.
+        if (id.length() != 8 ){
+            throw new IllegalIDException("ID must be eight characters");
+        }
+    }
 
 
 
