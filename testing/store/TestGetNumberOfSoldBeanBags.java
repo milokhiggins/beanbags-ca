7b2a4729a3d880c3ca3cc022398d44f7acdc24b9
Sarah Joy Brittain <S.brittain-16@student.lboro.ac.uk>
2020-03-10 18:40:08 +0000

    startinghele;zfj

--- testing/store/TestGetNumberOfSoldBeanBags.java
+++ testing/store/TestGetNumberOfSoldBeanBags.java
@@ -1,10 +1,16 @@
 package testing.store;
 
 import testing.Unittest;
+import beanbags.*;
 
 public class TestGetNumberOfSoldBeanBags implements Unittest {
-    public TestGetNumberOfSoldBeanBags() {
-    }
-    public void run() {
+    public void run() throws IllegalIDException, BeanBagMismatchException, InvalidMonthException, IllegalNumberOfBeanBagsAddedException, PriceNotSetException, BeanBagIDNotRecognisedException, BeanBagNotInStockException, InsufficientStockException, IllegalNumberOfBeanBagsSoldException, InvalidPriceException {
+        Store store = new Store();
+        assert store.getNumberOfSoldBeanBags() == 0 : "number of sold bean bags initialises incorrectly.";
+        store.addBeanBags(5, "manufacturer", "Sarah'sBean", "128AB346",
+                (short) 1994, (byte) 1, "hello!");
+        store.setBeanBagPrice("128AB346", 200);
+        store.sellBeanBags(5, "128AB346");
+        assert store.getNumberOfSoldBeanBags() == 5 : "number of sold bean bags faulty";
     }
 }
